cfaef802b671f7704a07dd6253174114ec3fe4ff
Maksud Bhaldar <macbhaldar@gmail.com>
2022-05-23 19:39:46 +0530

    Create PyTorch_backpropagation.py

---
+++ PyTorch_backpropagation.py
@@ -0,0 +1,24 @@
+import torch
+
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+w = torch.tensor(1.0, requires_grad=True)
+
+# forward pass to compute loss
+y_predicted = w * x
+loss = (y_predicted - y)**2
+print(loss)
+
+# backward pass to compute gradient dLoss/dw
+loss.backward()
+print(w.grad)
+
+# update weights
+
+# continue optimizing:
+# update weights, this operation should not be part of the computational graph
+with torch.no_grad():
+    w -= 0.01 * w.grad
+# don't forget to zero the gradients
+w.grad.zero_()
