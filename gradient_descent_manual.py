35246a79df3ee93004f9527afc5fd9899fd76905
Maksud Bhaldar <macbhaldar@gmail.com>
2022-05-23 22:31:59 +0530

    Update gradient_descent_manual.py

--- gradient_descent_manual.py
+++ gradient_descent_manual.py
@@ -1,11 +1,8 @@
 import numpy as np 
 
-# Compute every step manually
-
 # Linear regression
 # f = w * x 
 
-# here : f = 2 * x
 X = np.array([1, 2, 3, 4], dtype=np.float32)
 Y = np.array([2, 4, 6, 8], dtype=np.float32)
 
